79a8511c0c0f32fcbd7deffcfb98ef02eac6ab53
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-03-27 19:40:52 -0400

    Recategorizing Items

---
+++ Prototype_1/js/destination.js
@@ -0,0 +1,19 @@
+// Will Allow me to create pages for multiple destinations
+class location {
+    constructor(rollType, rollGlazing, packSize, rollPrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = rollPrice;    
+        
+    }
+};
+
+// First Destination Data
+const destinations = {
+    "Sanrio Land": {
+        "Description": 2.49,
+        "MainImageFile": "bakery_products/original-cinnamon-roll.jpg"
+    },
+   
+};
